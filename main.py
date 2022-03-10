7de86fbbe0fac77bff219fb57a9c6f333fdf3695
Jakub Iwaszek <jakubiwaszek@Jakubs-MacBook-Pro.local>
2022-03-10 21:06:10 +0100

    added methods to get page data from url, added logger and requests

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
+import requests
+import logging
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods=["GET"])
 
 def index():
-    return 'Index'
+    global requestedUrl, tag
+    #TODO: Implement mechanism to get only opinions, not the whole site
+    requestedUrl = 'https://www.ceneo.pl/94823130;02514?tag=producenci-philips#tab=reviews'
+    tag = 'div'
+    page = requests.get(requestedUrl)
+    return page.text
+
+if __name__ == '__main__':
+    app.run(debug=True)

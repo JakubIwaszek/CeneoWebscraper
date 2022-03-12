577e90f63d68643b778c81281962ab6f5181e7e4
Jakub Iwaszek <jakubiwaszek@Jakubs-MacBook-Pro.local>
2022-03-12 13:21:46 +0100

    adjusted Review module import, created getLogData method, added scrapReview function

--- Review.py
+++ Review.py
@@ -1,7 +1,10 @@
-class Review():
+class ReviewComment():
     def __init__(self, id, authorName, productRate, commentText, recommendation):
         self.id = id
         self.authorName = authorName
         self.productRate = productRate
         self.commentText = commentText
         self.recommendation = recommendation
+
+    def getLogData(self):
+        return "\n Review id: " + self.id + "\n Author name: " + self.authorName + " \n Product rate: " + self.productRate + "\n Comment: " + self.commentText + "\n Recommendation: " + self.recommendation
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from flask import (
 )
 import requests
 import logging
+import Review as Review
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-@app.route('/', methods=["GET"])
 
+
+@app.route('/', methods=["GET"])
 def index():
     global requestedUrl
     page = 1
@@ -19,16 +21,13 @@ def index():
         requestedUrl = f'https://www.ceneo.pl/94823130/opinie-{page}'
         requestedPage = requests.get(requestedUrl)
         soup = BeautifulSoup(requestedPage.content, "html.parser")
-        allCommentsSection = soup.find(class_="js_product-reviews js_reviews-hook js_product-reviews-container")
-        comments = allCommentsSection.find_all("div", class_="user-post user-post__card js_product-review")
+        allCommentsSection = soup.find(
+            class_="js_product-reviews js_reviews-hook js_product-reviews-container")
+        comments = allCommentsSection.find_all(
+            "div", class_="user-post user-post__card js_product-review")
         for comment in comments:
-            reviewId = comment.get('data-entry-id')
-            authorName = comment.find("span", class_="user-post__author-name").text
-            productRate = comment.find("span", class_="user-post__score-count").text
-            commentContent = comment.find("div", class_="user-post__text").text
-            recommendation = comment.find("span", class_="user-post__author-recomendation").text
-            # app.logger.info(reviewId + authorName + "  ^  " + productRate + "  ^  " + commentContent + " ^ " + recommendation)
-            # app.logger.info(authorName)
+            review = scrapReview(comment)
+            app.logger.info(review.getLogData())
         if len(comments) < 10:
             hasNextPage = False
         totalReviews += len(comments)
@@ -36,8 +35,15 @@ def index():
         app.logger.info(totalReviews)
     return "results"
 
+def scrapReview(comment):
+    reviewId = comment.get('data-entry-id')
+    authorName = comment.find("span", class_="user-post__author-name").text
+    productRate = comment.find(
+        "span", class_="user-post__score-count").text
+    commentContent = comment.find("div", class_="user-post__text").text
+    recommendation = comment.find(
+        "span", class_="user-post__author-recomendation").text
+    return Review.ReviewComment(reviewId, authorName, productRate, commentContent, recommendation)
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-def scrapReview(comment):
-    

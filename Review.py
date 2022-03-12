26283b6e74555407fa809316749f12f96c8601f6
Jakub Iwaszek <jakubiwaszek@Jakubs-MacBook-Pro.local>
2022-03-12 14:27:09 +0100

    added variables: confirmedPurchase, publishedDate, purchaseDate

--- Review.py
+++ Review.py
@@ -1,10 +1,13 @@
 class ReviewComment():
-    def __init__(self, id, authorName, productRate, commentText, recommendation):
+    def __init__(self, id, authorName, productRate, commentText, recommendation, confirmedPurchase, publishedDate, purchasedDate):
         self.id = id
         self.authorName = authorName
         self.productRate = productRate
         self.commentText = commentText
         self.recommendation = recommendation
+        self.confirmedPurchase = confirmedPurchase
+        self.publishedDate = publishedDate
+        self.purchasedDate = purchasedDate
 
     def getLogData(self):
-        return "\n Review id: " + self.id + "\n Author name: " + self.authorName + " \n Product rate: " + self.productRate + "\n Comment: " + self.commentText + "\n Recommendation: " + self.recommendation
+        return "\n Review id: " + self.id + "\n Author name: " + self.authorName + " \n Product rate: " + self.productRate + "\n Comment: " + self.commentText + "\n Recommendation: " + self.recommendation + "\n Confirmed purchase: " + self.confirmedPurchase + "\n Published date: " + self.publishedDate + " - Purchased date: " + self.purchasedDate
--- main.py
+++ main.py
@@ -17,14 +17,12 @@ def index():
     hasNextPage = True
     totalReviews = 0
     while(hasNextPage):
-        app.logger.info(page)
+        # app.logger.info(page)
         requestedUrl = f'https://www.ceneo.pl/94823130/opinie-{page}'
         requestedPage = requests.get(requestedUrl)
         soup = BeautifulSoup(requestedPage.content, "html.parser")
-        allCommentsSection = soup.find(
-            class_="js_product-reviews js_reviews-hook js_product-reviews-container")
-        comments = allCommentsSection.find_all(
-            "div", class_="user-post user-post__card js_product-review")
+        allCommentsSection = soup.find(class_="js_product-reviews js_reviews-hook js_product-reviews-container")
+        comments = allCommentsSection.find_all("div", class_="user-post user-post__card js_product-review")
         for comment in comments:
             review = scrapReview(comment)
             app.logger.info(review.getLogData())
@@ -32,18 +30,24 @@ def index():
             hasNextPage = False
         totalReviews += len(comments)
         page += 1
-        app.logger.info(totalReviews)
+        # app.logger.info(totalReviews)
     return "results"
 
 def scrapReview(comment):
     reviewId = comment.get('data-entry-id')
     authorName = comment.find("span", class_="user-post__author-name").text
-    productRate = comment.find(
-        "span", class_="user-post__score-count").text
+    productRate = comment.find("span", class_="user-post__score-count").text
     commentContent = comment.find("div", class_="user-post__text").text
-    recommendation = comment.find(
-        "span", class_="user-post__author-recomendation").text
-    return Review.ReviewComment(reviewId, authorName, productRate, commentContent, recommendation)
+    recommendation = comment.find("span", class_="user-post__author-recomendation").text
+    confirmedPurchase = "Opinia nie potwierdzona zakupem"
+    if comment.find("div", class_="review-pz"):
+        confirmedPurchase = comment.find("div", class_="review-pz").text
+    dates = comment.find("span", class_="user-post__published").find_all("time")
+    publishedDate = dates[0].get("datetime")
+    purchasedDate = ""
+    if len(dates) > 1:
+        purchasedDate = dates[1].get("datetime")
+    return Review.ReviewComment(reviewId, authorName, productRate, commentContent, recommendation, confirmedPurchase, publishedDate, purchasedDate)
 
 if __name__ == '__main__':
     app.run(debug=True)
